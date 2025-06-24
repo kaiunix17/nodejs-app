aad7d55169b9cc6cf176487f521fa74d98fa3a5c
kaiunix17 <ismoilovshohabbos25@gmail.com>
2025-06-24 13:29:32 +0500

    ukrainebek

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // Define a simple route that returns "Hello World"
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send('Ukraine, Kyiv');
 });
 
 // Define a route that returns a message
